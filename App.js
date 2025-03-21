f7bd0e3034851bcb71e9b796cf62154e04360773
nmartinez71 <nmartinez71@ivytech.edu>
2025-03-20 22:48:34 -0400

    first repo
    first repo

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hi, my name is Neftali Martinez, and I am a mobile developer</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
